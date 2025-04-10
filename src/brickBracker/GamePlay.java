dbb4192eb9337521641b6b85c77d100f9cf85a5a
omorsultan <omorsultansust@gmail.com>
2025-04-10 15:14:33 +0600

    complete

--- src/brickBracker/GamePlay.java
+++ src/brickBracker/GamePlay.java
@@ -36,28 +36,42 @@ public class GamePlay extends JPanel implements KeyListener , ActionListener {
         timer.start();
 
     }
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         //background
         g.setColor(Color.black);
-        g.fillRect(1,1,692,592);
+        g.fillRect(1, 1, 692, 592);
 
         //drawing map
-        map.draw((Graphics2D)g);
+        map.draw((Graphics2D) g);
 
         //borders
         g.setColor(Color.yellow);
-        g.fillRect(0,0,3,592);
-        g.fillRect(0,0,692,3);
-        g.fillRect(691,0,3,592);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        //scores
+        g.setColor(Color.white);
+        g.setFont(new Font("serif", Font.BOLD, 25));
+        g.drawString("Score is " + score, 500, 30);
 
         // the paddale
         g.setColor(Color.green);
-        g.fillRect(playerX,550,100,8);
+        g.fillRect(playerX, 550, 100, 8);
 
         // the ball
         g.setColor(Color.yellow);
-        g.fillOval(ballPosX,ballPosY,20,20);
+        g.fillOval(ballPosX, ballPosY, 20, 20);
+
+        if (ballPosY > 570)
+        {
+            play = false;
+            ballXdir = 0;
+            ballYdir = 0;
+            g.setColor(Color.RED);
+            g.setFont(new Font("serif",Font.BOLD,30));
+            g.drawString("Game Over ! \n Score is : "+score,220,280);
+        }
 
         g.dispose();
     }
